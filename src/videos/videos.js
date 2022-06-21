57edf7e35c90510afec5a5b5073591920a9e67f0
Maxim <kejerino@yandex.ru>
2022-06-21 21:02:24 +0800

    fix: correcting the test type

--- src/videos/videos.js
+++ src/videos/videos.js
@@ -1,9 +1,10 @@
 import video_1 from './items/video_1.mp4'
-import video_2 from './items/video_2.webm'
+import video_2 from './items/video_2.mp4'
 import video_3 from './items/video_3.mp4'
+import badVideo from './items/test.jpeg' // тест на неправильный формат "видео"
 
-const videosArray = [video_1, video_2, video_3]
-export const typesArray = ['mp4', 'webm', 'avi']
+const videosArray = [video_1, badVideo, video_2, video_3]
+const typesArray = ['mp4', 'webm', 'avi']
 
 // Проверка на корретный тип видео
 export const videos =  videosArray.filter((item) => {
