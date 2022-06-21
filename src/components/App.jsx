11754aedd8d19a8a44934b03d7905c2ec690548b
Maxim <kejerino@yandex.ru>
2022-06-21 20:34:39 +0800

    feat!: added current type for video

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,19 +4,20 @@ import {useEffect, useRef, useState} from "react";
 import Panel from "./Panel";
 import AlertError from "./AlertError";
 import {videos} from "../videos/videos"
-import {typesArray} from "../videos/videos"
+import {getTypeVideo} from "../videos/videos"
 import Video from "./Video";
 
 const App = () => {
     const videoRef = useRef(null); // реф плеера
-    // const [currentType, setCurrentType] = useState(typesArray[0]) // тип обрабатываемого видео
+    const [currentType, setCurrentType] = useState('mp4') // тип обрабатываемого видео
     const [duration, setDuration] = useState(0); // длина видео
     const [index, setIndex] = useState(0); // индекс текущего видео в массиве
     const [time, setTime] = useState(null); // текущее время видео
-    const valueDanger = 12; // количество секунд до появления красной зоны прогресс бара
+    const valueDanger = 10; // количество секунд до появления красной зоны прогресс бара
 
     // Ожидание загрузки и получение длины видео
     useEffect(() => {
+        setCurrentType(getTypeVideo(index))
         if (videoRef.current.src) {
             const video = videoRef.current
             video.onloadedmetadata = function () {
@@ -67,12 +68,14 @@ const App = () => {
     // Обработка успешного клика на кнопку
     function handleNextVideo() {
         setIndex(getNextIndex(index))
+        setCurrentType(getTypeVideo(index))
         setTime(null)
     }
 
     function handleResetVideos() {
         setTime(null)
         setIndex(0)
+        setCurrentType(getTypeVideo(index))
         playVideo();
     }
 
@@ -91,6 +94,7 @@ const App = () => {
                     index={index}
                     videoRef={videoRef}
                     videos={videos}
+                    currentType={currentType}
                 />
                 <Panel
                     duration={duration}
